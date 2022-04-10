d580f5f3189cda2788a97d762bbebd402049c6b5
flanderos <andershellerud@hotmail.no>
2022-04-10 22:23:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,76 @@
 const cats = [
-    {
-      name: "Blob",
-      age: 10,
-    },
-    {
-      name: "Harold",
-    },
-    {
-      name: "Blurt",
-      age: 21,
-    },
-  ];
-  
-  let ageOfCat = cats.age;
-  
-  // Question 1
-  
-  const logTheWord = [(complain = logWord)];
-  
-  function logWord() {
-    console.log("meow");
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
+];
+
+let ageOfCat = cats.age;
+
+// Question 1
+
+const logTheWord = [(complain = logWord)];
+
+function logWord() {
+  console.log("meow");
+}
+
+logWord();
+
+// Question 2
+
+let heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated Heading";
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// Question 4
+
+heading.classList.add("subheading");
+
+// Question 5
+
+let paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++) {
+  p = paragraphs[i].style.color = "red";
+}
+
+// Question 6
+
+let resultContainer = document.querySelector(".results");
+resultContainer.innerHTML = "<p>New paragraph</p>";
+
+resultContainer.style.backgroundColor = "yellow";
+
+// Question 7
+
+function listFunction(list) {
+  for (let i = 0; i < list.length; i++) {
+    console.log(list[i].name);
   }
-  
-  logWord();
-  
-  // Question 2
-  
-  let heading = document.querySelector("h3");
-  
-  heading.innerHTML = "Updated Heading";
-  
-  // Question 3
-  
-  heading.style.fontSize = "2em";
-  
-  // Question 4
-  
-  heading.classList.add("subheading");
-  
-  // Question 5
-  
-  let paragraphs = document.querySelectorAll("p");
-  
-  for (let i = 0; i < paragraphs.length; i++) {
-    p = paragraphs[i].style.color = "red";
+}
+
+listFunction(cats);
+
+// Question 8
+
+function createCats(cats) {
+  for (let i = 0; i < cats.length; i++) {
+    let catList = JSON.stringify(cats);
+    document.querySelector(".cat-container").innerHTML =
+      "<div>" + catList + "</div>";
   }
-  
-  // Question 6
-  
-  let resultContainer = document.querySelector(".results");
-  resultContainer.innerHTML = "<p>New paragraph</p>";
-  
-  resultContainer.style.backgroundColor = "yellow";
-  
-  // Question 7
-  
-  function listFunction(list) {
-    for (let i = 0; i < list.length; i++) {
-      console.log(list[i].name);
-    }
-  }
-  
-  listFunction(cats);
-  
-  // Question 8
-  
-  
+}
+
+createCats(cats);
